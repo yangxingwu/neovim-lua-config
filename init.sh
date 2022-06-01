b9a0f8977a06f13cc34d720bba75153e9922ade8
yangxingwu <xingwu.yang@gmail.com>
2022-06-01 10:06:36 +0000

    install packer manually

---
+++ init.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# link nvim config script
+mkdir -p ~/.config/nvim
+ln -s "${PWD}"/init.lua ~/.config/nvim/init.lua
+ln -s "${PWD}"/lua      ~/.config/nvim/lua
+
+# install packer.vim
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+    ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+# ripgrep is a line-oriented search tool that recursively searches the current
+# directory for a regex pattern
+# https://github.com/BurntSushi/ripgrep
+sudo apt install ripgrep
+
+# fd is a program to find entries in your filesystem
+# https://github.com/sharkdp/fd
+sudo apt install fd-find
+
+# update nodejs
+sudo npm install -g n
+sudo n lts # Long Term Support (or sudo n latest)
+
+# install tree-sitter
+sudo npm install -g tree-sitter-cli
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,10 +1,3 @@
--- Install packer automatically
-local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
-
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = vim.fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-end
-
 -- Automatically regenerate compiled loader file
 local packer_group = vim.api.nvim_create_augroup('Packer', { clear = true })
 vim.api.nvim_create_autocmd('BufWritePost', { command = 'source <afile> | PackerCompile', group = packer_group, pattern = 'plugins.lua' })
