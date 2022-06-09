9a8fb73324c2b7a47984e3983f936c193845dad7
yangxingwu <xingwu.yang@gmail.com>
2022-06-09 09:20:49 +0000

    telescope: add key mappings

--- lua/configs/telescope.lua
+++ lua/configs/telescope.lua
@@ -1,5 +1,23 @@
 require('telescope').setup {
+  defaults = {
+    -- where {mode} is the one character letter for a mode
+    -- ('i' for insert, 'n' for normal).
+    -- check lua/telescope/mappings.lua for more details
+    mappings = {
+      i = {
+        -- ['<C-u>'] = false,
+        -- ['<C-d>'] = false,
+      },
+    },
+  },
 }
 
 -- get fzf loaded and working with telescope
 require('telescope').load_extension('fzf')
+
+-- add leader shortcuts
+local opts = { noremap=true, silent=true }
+vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files, opts)
+vim.keymap.set('n', '<leader>fg', require('telescope.builtin').live_grep, opts)
+vim.keymap.set('n', '<leader>fb', require('telescope.builtin').buffers, opts)
+vim.keymap.set('n', '<leader>fh', require('telescope.builtin').help_tags, opts)
