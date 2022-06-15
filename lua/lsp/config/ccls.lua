6bbd9d93d733df1a19740b19a537cab930fe6bd1
yangxingwu <xingwu.yang@gmail.com>
2022-06-15 09:53:09 +0000

    add ccls lsp server

---
+++ lua/lsp/config/ccls.lua
@@ -0,0 +1,19 @@
+require('lsp.common')
+
+require('lspconfig').ccls.setup {
+  -- https://github.com/neovim/nvim-lspconfig#suggested-configuration
+  on_attach = LSP_ON_ATTACH,
+  flags = {
+    -- This will be the default in neovim 0.7+
+    debounce_text_changes = 150,
+  },
+
+  -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
+  capabilities = LSP_CLIENT_CAPABILITIES,
+
+  init_options = {
+    cache = {
+      retainInMemory = 0,
+    },
+  },
+}
