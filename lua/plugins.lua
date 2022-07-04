bb45c11aa792280350ac5b162469f3ec641d82bd
yangxingwu <xingwu.yang@gmail.com>
2022-07-04 11:14:17 +0000

    plugins.lua: reformat as kickstart.nvim/init.lua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -5,38 +5,17 @@ vim.api.nvim_create_autocmd('BufWritePost', { command = 'source <afile> | Packer
 local use = require('packer').use
 
 require('packer').startup(function()
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
-
-  -- gruvbox theme
-  use { 'ellisonleao/gruvbox.nvim', requires = 'rktjmp/lush.nvim' }
-
-  -- bufferline
-  use { 'akinsho/bufferline.nvim', requires = 'kyazdani42/nvim-web-devicons' }
-
-  -- Fancier statusline
-  use 'nvim-lualine/lualine.nvim'
-
-  -- nvim-tree
-  use { 'kyazdani42/nvim-tree.lua', requires = 'kyazdani42/nvim-web-devicons' }
-
-  -- treesitter
-  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-
-  -- language server protocol support
-  use 'neovim/nvim-lspconfig'
-  use 'williamboman/nvim-lsp-installer'
-
-  -- auto completion
-  use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
-  use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
-  use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
-  use 'L3MON4D3/LuaSnip' -- Snippets plugin
-
-  -- Add git related info in the signs columns and popups
-  use { 'lewis6991/gitsigns.nvim', requires = 'nvim-lua/plenary.nvim' }
-
-  -- UI to select things (files, grep results, open buffers...)
-  use { 'nvim-telescope/telescope.nvim', requires = 'nvim-lua/plenary.nvim' }
-  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+  use 'wbthomason/packer.nvim'                                                          -- Package manager
+  use { 'ellisonleao/gruvbox.nvim', requires = { 'rktjmp/lush.nvim' } }                 -- gruvbox theme
+  use { 'akinsho/bufferline.nvim', requires = { 'kyazdani42/nvim-web-devicons' } }      -- A snazzy buffer line
+  use 'nvim-lualine/lualine.nvim'                                                       -- Fancier statusline
+  use { 'kyazdani42/nvim-tree.lua', requires = { 'kyazdani42/nvim-web-devicons' } }     -- File Explorer
+  use 'nvim-treesitter/nvim-treesitter'                                                 -- Highlight, edit, and navigate code
+  use 'neovim/nvim-lspconfig'                                                           -- Collection of configurations for built-in LSP client
+  use 'williamboman/nvim-lsp-installer'                                                 -- Automatically install language servers to stdpath
+  use { 'hrsh7th/nvim-cmp', requires = { 'hrsh7th/cmp-nvim-lsp' } }                     -- Autocompletion
+  use { 'L3MON4D3/LuaSnip', requires = { 'saadparwaiz1/cmp_luasnip' } }                 -- Snippet Engine and Snippet Expansion
+  use { 'lewis6991/gitsigns.nvim', requires = 'nvim-lua/plenary.nvim' }                 -- Add git related info in the signs columns and popups
+  use { 'nvim-telescope/telescope.nvim', requires = 'nvim-lua/plenary.nvim' }           -- Fuzzy Finder (files, lsp, etc)
+  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }                      -- Fuzzy Finder Algorithm
 end)
