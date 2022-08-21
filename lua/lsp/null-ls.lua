4a06ea551385fd23a0c432fe6fe3f21caa264834
yangxingwu <xingwu.yang@gmail.com>
2022-08-21 06:03:54 +0000

    python: add flake8 and black for code format and lint

--- lua/lsp/null-ls.lua
+++ lua/lsp/null-ls.lua
@@ -1,6 +1,11 @@
 require("null-ls").setup {
   sources = {
+    -- for bash
     require("null-ls").builtins.formatting.shfmt,
     require("null-ls").builtins.code_actions.shellcheck,
+
+    -- for python
+    require("null-ls").builtins.formatting.black,
+    require("null-ls").builtins.diagnostics.flake8,
   },
 }
