0ed70af31846207bdfbc50f8fd9ecfc51d7b2354
yangxingwu <xingwu.yang@gmail.com>
2022-08-21 20:49:13 +0800

    python: use black with line length 79
    as consistent with PEP8

--- lua/lsp/null-ls.lua
+++ lua/lsp/null-ls.lua
@@ -5,7 +5,9 @@ require("null-ls").setup {
     require("null-ls").builtins.code_actions.shellcheck,
 
     -- for python
-    require("null-ls").builtins.formatting.black,
+    require("null-ls").builtins.formatting.black.with {
+      extra_args = { "--line-length", "79" },
+    },
     require("null-ls").builtins.diagnostics.flake8,
   },
 }
