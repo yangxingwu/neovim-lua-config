f5720f7331cf97de50868faa8e339cbc8669a933
yangxingwu <xingwu.yang@gmail.com>
2023-06-14 06:02:57 +0000

    gitsigns: git blame line under current cursor by default

--- lua/configs/gitsigns.lua
+++ lua/configs/gitsigns.lua
@@ -6,4 +6,12 @@ require('gitsigns').setup {
     topdelete = { text = '‾' },
     changedelete = { text = '~' },
   },
+  current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+  current_line_blame_opts = {
+    virt_text = true,
+    virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+    delay = 10,
+    ignore_whitespace = false,
+  },
+  current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
 }
