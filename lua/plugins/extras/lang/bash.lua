c3b892fa261407049153f5d11109bcd7aa94e682
yangxingwu <xingwu.yang@gmail.com>
2023-09-05 14:12:24 +0800

    Feat/lsp: add shellcheck for bash

---
+++ lua/plugins/extras/lang/bash.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "shellcheck",
+      },
+    },
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      opts.sources = opts.sources or {}
+      vim.list_extend(opts.sources, {
+        nls.builtins.diagnostics.shellcheck,
+        nls.builtins.code_actions.shellcheck,
+      })
+    end,
+  },
+}
