19617686de0138c903c6fef36c57fc624b09ee34
yangxingwu <xingwu.yang@gmail.com>
2023-09-05 14:08:51 +0800

    Fix/lsp: language specific config takes no effect

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -20,6 +20,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.rust" },
     -- import/override with your plugins
     { import = "plugins" },
+    { import = "plugins.extras.lang" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
