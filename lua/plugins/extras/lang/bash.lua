f6254979786a438f3bdf0c4d7f2c1bb571c5f80a
yangxingwu <xingwu.yang@gmail.com>
2023-10-20 16:52:51 +0800

    Fix/lsp: add bash-language-server for bash

--- lua/plugins/extras/lang/bash.lua
+++ lua/plugins/extras/lang/bash.lua
@@ -1,5 +1,12 @@
 return {
   {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "bash-language-server" })
+    end,
+  },
+  {
     "mfussenegger/nvim-lint",
     optional = true,
     opts = {
