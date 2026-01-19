e687e0745665f49f98d3d4d73ab69ed1a88d54fe
yangxingwu <xingwu.yang@gmail.com>
2026-01-19 21:01:30 +0800

    feat: add monokai-pro color scheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,14 @@
 return {
   -- add gruvbox
   { "ellisonleao/gruvbox.nvim" },
+  -- add monokai-pro
+  { "loctvl842/monokai-pro.nvim" },
 
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "monokai-pro",
     },
   },
 }
