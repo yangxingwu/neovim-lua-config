4339cea72c27f3a57e2ceda618205d82661be572
yangxingwu <xingwu.yang@gmail.com>
2026-01-20 19:49:17 +0800

    feat: set default colorscheme to catppuccin-mocha

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,14 +1,12 @@
 return {
   -- add gruvbox
   { "ellisonleao/gruvbox.nvim" },
-  -- add monokai-pro
-  { "loctvl842/monokai-pro.nvim" },
 
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "monokai-pro",
+      colorscheme = "catppuccin-mocha",
     },
   },
 }
