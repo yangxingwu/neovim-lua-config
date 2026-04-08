b0c4e11d791bd86747677158e34f591d3dfa5c0b
yangxingwu <xingwu.yang@gmail.com>
2026-04-08 14:55:16 +0800

    meson: switch LSP from mesonlsp to muon
    muon is actively maintained while mesonlsp has been archived.

--- lua/plugins/meson.lua
+++ lua/plugins/meson.lua
@@ -1,7 +1,7 @@
 return {
+  -- Use muon as meson LSP (actively maintained, replaces archived mesonlsp)
   "neovim/nvim-lspconfig",
-  -- enable meson lsp
   opts = {
-    servers = { mesonlsp = {} },
+    servers = { muon = {} },
   },
 }
